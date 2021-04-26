1a51339f3597eae4743aa2caa37d4390ff39759c
joelomar15 <82985329+joelomar15@users.noreply.github.com>
2021-04-26 18:48:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards',{delay:1000});
+ScrollReveal().reveal('.showcase',{delay:1000});
+ScrollReveal().reveal('.cards-banner-uno',{delay:1000});
+
